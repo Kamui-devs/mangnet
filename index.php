2f4090c26e7349a434fc785be8a002df41e9dca6
eduardo <eduardo.sa@acad.ifma.edu.br>
2020-06-08 17:27:11 -0300

    cabeçalho e rodapé

---
+++ index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Mangnet</title>
+	<link rel="stylesheet" type="text/css" href="_css/estilo.css">
+	<link href="https://fonts.googleapis.com/css2?family=Source+Sans+Pro:wght@300&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Hind:wght@300&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div id="main">
+		<header id="cabecalho">
+			<figure id="logo">
+				<img src="_fotos/f1.png">
+			</figure>
+			<nav id="menu">
+				<ul>
+					<li><a href="">Home</a></li>
+					<li><a href="">Nossa História</a></li>
+					<li><a href="">Planos</a></li>
+					<li><a href="">Área de Cobertura</a></li>
+					<li><a href="">Suporte/SAC</a></li>
+				</ul>	
+			</nav>
+		</header>
+		<section id="conteudo">
+			<article class="esquerda">
+				<figure class="e"><img src="_fotos/f3.png"></figure>
+				<aside class="texto1">
+					<h1>Agora estamos com fibra Óptica</h1>
+					<p>30 mb de internet da Embratel</p>
+				</aside>
+			</article class="direita">
+			<figure class="d"><img src="_fotos/f4.png"></figure>
+			<aside class="texto2">
+				<h1>Visite nossa loja off</h1>
+				<p>Variedade em produtos eletrônicos</p>
+				<p>localizada em S.R. das Mangabeiras-MA</p>
+			</aside>
+		</section>
+		<footer id="rodape">
+			<p>&copy;Kamui Devs - 2020</p>
+  			<a href="https://github.com/Kamui-devs/mangnet">
+  				<img src="https://marcas-logos.net/wp-content/uploads/2020/03/GITHUB-LOGO.png">
+  			</a>
+		</footer>
+	</div>
+</body>
+</html>
